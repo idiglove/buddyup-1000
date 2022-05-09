af48bf49e83893d197529518a55c2c3729d2ce42
Faith <faith.morante@gmail.com>
2022-05-09 17:45:50 +0800

    lawnmower pull up string

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{ 
+    public Vector3 gameObjectSreenPoint;
+    public Vector3 mousePreviousLocation;
+    public Vector3 mouseCurLocation;
+    public Rigidbody2D rb;
+    bool isMoving = false;
+
+    private void Start() {
+        rb = transform.parent.GetComponent<Rigidbody2D>();
+        // rb = GetComponent<Rigidbody2D>();
+        // rb.AddForce(Vector3.up * 1000f, ForceMode2D.Impulse);
+        // StartCoroutine(MoveObjectOverTime(transform.parent, new Vector3(2f,2f), 10f ));
+        
+    }
+
+}
--- Assets/Scripts/LawnMower.cs
+++ Assets/Scripts/LawnMower.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LawnMower : MonoBehaviour
 {
     public GameObject selectedObject;
     Vector3 offset;
+    Vector3 startPos;
+    Vector3 startDrag;
+    Vector3 endDrag;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.Find("Hook").position;
     }
 
     // Update is called once per frame
@@ -22,9 +26,10 @@ public class LawnMower : MonoBehaviour
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
             if (targetObject && targetObject.name == "Square")
             {
-                Debug.Log("rope: " + targetObject.transform.parent.parent.localScale);
+                // Debug.Log("rope: " + targetObject.transform.parent.parent.localScale);
                 selectedObject = targetObject.transform.gameObject;
                 offset = selectedObject.transform.position - mousePosition;
+                startDrag = mousePosition;
             }
         }
         if (selectedObject)
@@ -37,6 +42,47 @@ public class LawnMower : MonoBehaviour
         {
             // on release, unselect the object
             selectedObject = null;
+            transform.Find("Hook").position = startPos;
+            endDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("startDrag: " + startDrag);
+            Debug.Log("endDrag: " + endDrag);
+            Debug.Log("magnitude: " + (endDrag - startDrag).magnitude);
+            float magnitude = (endDrag - startDrag).magnitude;
+            float random = Random.Range(10, 20);
+            GameObject.Find("RPM").GetComponent<TextMeshPro>().text = (magnitude * random).ToString();
         }
     }
+
+    // void FixedUpdate() {
+    //     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         // on mouse click, set the selected object and get offset
+    //         Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+    //         if (targetObject && targetObject.name == "Square")
+    //         {
+                
+    //             selectedObject = targetObject.transform.gameObject;
+    //             Debug.Log("rope: " + selectedObject.transform.parent.right);
+    //             offset = selectedObject.transform.position - mousePosition;
+    //             // selectedObject.transform.parent.GetComponent<Rigidbody2D>().AddForce(selectedObject.transform.parent.up * 1000f, ForceMode2D.Impulse);
+    //         }
+    //     }
+    //     if (selectedObject)
+    //     {
+    //         // set object's position based on mouse position
+    //         // selectedObject.transform.position = mousePosition + offset;
+            
+            
+
+    //         Vector3 forceGravity = new Vector3(1f, -1.5f, 0f);
+    //         // selectedObject.transform.parent.position = (mousePosition + offset) + (forceGravity * Time.fixedDeltaTime);
+    //         selectedObject.transform.parent.GetComponent<Rigidbody2D>().AddForce(transform.right * 100f);
+    //     }
+    //     if (Input.GetMouseButtonUp(0) && selectedObject)
+    //     {
+    //         // on release, unselect the object
+    //         selectedObject = null;
+    //     }
+    // }
 }
